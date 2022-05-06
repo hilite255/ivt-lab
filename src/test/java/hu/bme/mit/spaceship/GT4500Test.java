27dde8372a3a61d467ba0b6f577783ada56c016b
Horváth Tamás <79797280+hilite255@users.noreply.github.com>
2022-05-06 10:48:06 +0200

    Update GT4500Test.java

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,12 +9,14 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primary;
+  private TorpedoStore secondary;
 
   @BeforeEach
   public void init(){
-    var primaryMock = mock(TorpedoStore.class);
-    var secondaryMock = mock(TorpedoStore.class);
-    this.ship = new GT4500(primaryMock, secondaryMock);
+    primary = mock(TorpedoStore.class);
+    secondary = mock(TorpedoStore.class);
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
